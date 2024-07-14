0762914936233ebd987aecc2d29c9731b2014797
MauricioSQA <114277830+MauricioSQA@users.noreply.github.com>
2024-07-13 20:15:54 -0500

    Add files via upload

---
+++ src/main/java/com/foodappbackend/foodapp/entity/Usuario.java
@@ -0,0 +1,53 @@
+package com.foodappbackend.foodapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+
+    @Column(nullable = false)
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JsonIgnore
+    @JoinTable(
+            name = "usuarios_roles",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "rol_id")
+    )
+    @Enumerated(EnumType.STRING)
+    private Set<Rol> roles = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id) && Objects.equals(username, usuario.username) && Objects.equals(password, usuario.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password);
+    }
+}
